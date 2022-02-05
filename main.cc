65c2d83424a2adcbce517e609129c409268464ce
Nil Fons Miret <nilfm@Nils-MacBook-Pro.local>
2022-02-05 14:07:53 -0800

    Better solution

--- main.cc
+++ main.cc
@@ -3,43 +3,79 @@
 #include "cpp_utils/read_params.hh"
 #include "cpp_utils/write_output.hh"
 
+const int INF = 1e9;
+
 int main(int argc, char** argv) {
   map<string, double> p = read_params(argc, argv);
   read_data(argv[1]);
   srand(time(0));
-
+  int P = people.size();
   // chosen[i] == 1 if the i'th ingredient is chosen, else 0
   vector<int> chosen(N);
 
-  // assign score to ingredient
-  vector<int> scores(N);
-  for (Person& person : people) {
+  vector<int> ingr_scores(N);
+  for (Person person : people) {
     for (int like : person.likes) {
-      scores[like]++;
+      ingr_scores[like]++;
     }
     for (int dislike : person.dislikes) {
-      scores[dislike]--;
+      ingr_scores[dislike]--;
     }
   }
 
-  vector<int> ingredients(N);
-  for (int i = 0; i < N; i++) {
-    ingredients[i] = i;
+  vector<int> scores(P);
+  for (int i = 0; i < P; i++) {
+    Person person = people[i];
+    int a = person.likes.size();
+    int b = person.dislikes.size();
+    for (int ingr : person.likes) {
+      scores[i] += ingr_scores[ingr];
+    }
+    for (int ingr : person.dislikes) {
+      scores[i] -= ingr_scores[ingr];
+    }
+    if (a == 0 and b == 0) {
+      scores[i] = INF;
+    }
+    else {
+      scores[i] /= (a + b);
+    }
   }
 
-  sort(ingredients.begin(), ingredients.end(),
-       [&](int a, int b) -> bool { return scores[a] > scores[b]; });
+  vector<int> forbidden(N, 0);
 
-  int max_score = 1;
-  for (int score : scores) max_score = max(score, max_score);
+  vector<int> people_idx(P);
+  for (int i = 0; i < P; i++) {
+    people_idx[i] = i;
+  }
 
-  for (int ingr : ingredients) {
-    if (scores[ingr] > 0) {
-      // we want the best ingredient to have chance = threshold
-      // and the others to have chance = threshold * score / max_score
-      int rnd = rand() % max_score;
-      if (rnd < scores[ingr] * p["threshold"]) {
-        chosen[ingr] = 1;
+  sort(people_idx.begin(), people_idx.end(), [&](int a, int b) -> bool {
+    return scores[a] > scores[b];
+  });
+
+  for (int i = 0; i < P; i++) {
+    if (scores[people_idx[i]] == INF or rand() % 100 < p["threshold"] * 100) {
+      Person person = people[people_idx[i]];
+      bool can_do = true;
+      for (int ingr : person.dislikes) {
+        if (chosen[ingr]) {
+          can_do = false;
+          break;
+        }
+      }
+      for (int ingr : person.likes) {
+        if (forbidden[ingr]) {
+          can_do = false;
+          break;
+        }
+      }
+      if (can_do) {
+        for (int ingr : person.likes) {
+          chosen[ingr] = 1;
+        }
+        for (int ingr : person.dislikes) {
+          forbidden[ingr] = 1;
+        }
       }
     }
   }
